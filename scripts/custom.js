cd2881551e2605dec8b1e99606ff4baa9c077332
Suleman <mohdsulemanhussain@gmail.com>
2018-07-04 20:01:01 +0530

    basic project template
    basic project template

---
+++ scripts/custom.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav(); // initializing mobile view component
+    $(".dropdown-trigger").dropdown(); // initializing dropdown in navigation bar
+  });
+        
